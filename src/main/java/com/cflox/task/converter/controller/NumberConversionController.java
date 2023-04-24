8a098fcbe2f45915391f582643a5a41f5e38e6bb
shohelasad <shohel.asad@gmail.com>
2023-04-24 22:08:13 +0200

    add validation for conversion request

--- src/main/java/com/cflox/task/converter/controller/NumberConversionController.java
+++ src/main/java/com/cflox/task/converter/controller/NumberConversionController.java
@@ -4,6 +4,7 @@ import com.cflox.task.converter.dto.ConversionRequest;
 import com.cflox.task.converter.exception.ResourceNotFoundException;
 import com.cflox.task.converter.factory.ConversionFactory;
 import com.cflox.task.converter.service.ConversionService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class NumberConversionController {
     private ConversionService conversionService;
 
     @PostMapping("/conversions")
-    public String getConversion(@RequestBody ConversionRequest request){
+    public String getConversion(@RequestBody @Valid ConversionRequest request){
         conversionService = new ConversionFactory().process(request.getFrom(), request.getTo());
         if(conversionService == null) {
             throw new ResourceNotFoundException("Conversion not found for " + request.getFrom() + " -> " + request.getTo());
--- src/main/java/com/cflox/task/converter/dto/ConversionRequest.java
+++ src/main/java/com/cflox/task/converter/dto/ConversionRequest.java
@@ -1,6 +1,7 @@
 package com.cflox.task.converter.dto;
 
 import com.cflox.task.converter.enums.ConversionType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,9 +12,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ConversionRequest {
 
+    @NotNull
     private ConversionType from;
 
+    @NotNull
     private ConversionType to;
 
+    @NotNull
     private String number;
 }
