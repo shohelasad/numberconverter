fe5416b21f92520b6595ab9d99d75ba487ec10b0
shohelasad <shohel.asad@gmail.com>
2023-04-24 20:08:49 +0200

    add conversion classes

---
+++ src/main/java/com/cflox/task/converter/service/BinaryToRomanConversion.java
@@ -0,0 +1,19 @@
+package com.cflox.task.converter.service;
+
+
+import com.cflox.task.converter.enums.ConversionType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BinaryToRomanConversion implements ConversionService {
+
+    @Override
+    public boolean matches(ConversionType from, ConversionType to) {
+        return true;
+    }
+
+    @Override
+    public String convert(String input) {
+        return null;
+    }
+}
